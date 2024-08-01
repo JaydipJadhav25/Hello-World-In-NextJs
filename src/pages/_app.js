af3e6ab52843736bf462951f276074e0acf5ad20
jaydipjadhav <jaydipdjadhav>
2024-08-01 13:53:20 +0530

    add Custome page and api

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
+import { parseSetCookie } from "next/dist/compiled/@edge-runtime/cookies";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+     <h1>hello from Jaydip</h1>
+     <Component{...pageProps}/>
+     <h1>ThanK You </h1>
+  </>;
 }
---
+++ src/pages/jaydip.js
@@ -0,0 +1,3 @@
+export default function(){
+    return <h1>Hi , from Jaydip jadhav</h1>
+}
